df833c17a08d35bef400abb4c97734d84d52c2e1
justinemarco <justinejynnepatricemarco@student.laverdad.edu.ph>
2024-10-10 20:58:00 +0800

    I'm done editing the adding styles part

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,18 +1,10 @@
-function AboutPage() {
+function MyApp() {
   return (
     <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
-    </>
-  );
-}
-
-export default function MyApp() {
-  return (
-    <div>
       <h1>Welcome to my app</h1>
-      <AboutPage />
-    </div>
-  );
+      <img className="avatar" src='https://i.imgur.com/yXOvdOSs.jpg'/>
+    </>
+  )
 }
 
+export default MyApp
