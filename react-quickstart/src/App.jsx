8ed6239cc67e56fb5cc0602e45c3ca8f4088c1d3
justinemarco <justinejynnepatricemarco@student.laverdad.edu.ph>
2024-10-12 12:27:02 +0800

    I'm done using the map function to transform an array of products into an array of list items

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,17 +1,27 @@
 import './App.css'
-import AdminPanel from './AdminPanel';
-import LoginForm from './LoginForm';
 
 function MyApp() {
-  let isLoggedIn = true;
 
+  const products = [
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+  ];
+  
+  const listItems = products.map(product => 
+    <li key = {product.id}>
+      {product.title}
+    </li>
+  );
   
   return (
     <>
       <h1>Welcome to my app</h1>
-      {isLoggedIn && <AdminPanel />}
+      <ul>{listItems}</ul>
     </>
-  )
+  );
 }
+
+
 
 export default MyApp
